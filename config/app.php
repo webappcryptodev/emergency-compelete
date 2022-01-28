c7cd1d44686b6d9223ff495ee4209614982b7871
Bi Kong <94787970+WebManager0920@users.noreply.github.com>
2022-01-28 03:35:06 -0800

    w
    w

--- config/app.php
+++ config/app.php
@@ -120,7 +120,7 @@ return [
     */
     
     // 'key' => env('APP_KEY'),
-    'key' => env('APP_KEY', '3WyO92QfKvyKlWY1pxIHcAonSFXRBpdRSOufx+q+6Yo='),
+    'key' => env('APP_KEY', 'base64:3WyO92QfKvyKlWY1pxIHcAonSFXRBpdRSOufx+q+6Yo='),
     'cipher' => 'AES-256-CBC',
 
     /*
